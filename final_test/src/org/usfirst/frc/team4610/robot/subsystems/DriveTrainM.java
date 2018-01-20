c7eb0702cfda1f46f7ecbd6acc0820af3a0eee72
Nathan-Petrie <nathanrobotics1@gmail.com>
2018-01-19 20:35:02 -0600

    initial

---
+++ final_test/src/org/usfirst/frc/team4610/robot/subsystems/DriveTrainM.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team4610.robot.subsystems;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrainM extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
